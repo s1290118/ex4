49a1a777223607f40266619155b5afe2e3729c5a
s1290118 <s1290118@u-aizu.ac.jp>
2022-07-09 01:29:26 +0900

    User name added

--- Hotgame.c
+++ Hotgame.c
@@ -5,10 +5,12 @@ int main(){
   int Head=0,Tail=0;
   char name[10];
   int t;
+    printf("Who are you?\n>");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
   printf("Tossing a coin ...\n");
   for(int i=0;i<3;i++){
     printf("Round%d",i+1);
-
     t=rand()%2;
     if(t==0){
       Head++;
